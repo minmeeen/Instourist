363449c3825629960719f01bd7bbafd51f391258
minmeeen <sirapat.mp@gmail.com>
2023-01-07 21:59:58 +0700

    edit timezone and avoid repeat post

--- test1.py
+++ test1.py
@@ -2,33 +2,45 @@
 ### Around 50 posts allow for extraction ###
 ### Reason : ... ###
 import instaloader
-import datetime
+import datetime, pytz
 
 bot = instaloader.Instaloader()
 
 
-USER = "username"
-PASSWORD = "password"
+USER = "user"
+PASSWORD = "pass"
 # bot = instaloader.Instaloader(quiet=False, download_geotags=False, download_comments=False, save_metadata=False,compress_json=False)
 bot.login(USER, PASSWORD) 
 
 
-f = open("dataset\walkingstreet_d1-27_m12.txt", "a")
-l = bot.get_location_posts(121474598025096)
-# print(type(l))
-i = 1
+f = open("night_safari_d1-31_m12.txt", "w")
 
-start_date = datetime.datetime(2022, 12, 1)
-end_date = datetime.datetime(2022, 12, 31)
+i = 1
+s = True
+p = 1
+
+location_id = 195304697182096
+
+tz = pytz.timezone('Asia/Bangkok')
+start_date = datetime.datetime(2022, 12, 1, tzinfo=tz)
+end_date = datetime.datetime(2023,1, 1,tzinfo=tz)
+
+l = bot.get_location_posts(location_id)
 
 for post in l:
-    print(post.date)
-    if(start_date < post.date and post.date < end_date):
+    print(post.date_local)
+    if s == True:
+        fd = post.date_local
+        s = False
+
+    if(start_date <= post.date_local < end_date):
         f.write(post.owner_username)
         f.write('\n')
+        print(i)
         i+=1
-    else:
+    elif(post.date_local < start_date or (post.date_local==fd and p!=1)):
         break
+    p+=1
+print(p-1)
     
-    print(i)
 f.close()
