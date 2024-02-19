2f77273f0b2793c96cab08c0154d6e4fba3aaaba
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2024-02-19 20:14:27 +0700

    edit response getLanguageDetected

--- backend/common_service/src/service/BEservice.py
+++ backend/common_service/src/service/BEservice.py
@@ -30,21 +30,20 @@ def closeDB():
 #     except Exception as e :
 #         print(f"error : {e}")
 
+#create response model
 def getLanguageDetected(locationId, timestamp, duration):
     days = duration.split('D')
     currentTime = datetime.utcfromtimestamp(timestamp).date() #'yyyy-mm'dd'
     newTime = currentTime - timedelta(days=int(days[0]))
     languageDetected = db.findPostDetectedByDate(locationId, newTime, currentTime) #list of languages
-    print(languageDetected)
+    numOfPosts = len(languageDetected)
 
     languages = {}
-    response = {"Number of posts" : len(languageDetected), "Languages" : languages}
     for language in languageDetected :
         if language[0] in languages :
             languages[language[0]] += 1
         else :
             languages[language[0]] = 1
 
-    print(languages)
-    print(response)
+    response = {"Number of posts" : numOfPosts, "Languages" : languages}
     return response
