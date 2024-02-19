0cc85e192ee94c4af0257f5261dfd7775930ad87
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2024-02-20 01:10:32 +0700

    create LanguagesDetectedResponse model

--- backend/common_service/src/model.py
+++ backend/common_service/src/model.py
@@ -2,6 +2,8 @@ from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
+from typing import List
+from pydantic import BaseModel
 
 Base  = declarative_base()
 
@@ -23,7 +25,15 @@ class Location(Base):
     __tablename__ = 'location'
     location_id = Column(Integer, primary_key=True)
     location_name = Column(String)
-    ig_location = Column(Integer[])
+    # ig_location = Column(Integer[])
     # created_at = Column(DateTime(timezone=True), server_default=func.now())
     # updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
+class LangugesResponse(BaseModel):
+    languageName : str
+    total : int
+
+
+class LanguageDetectedResponse(BaseModel):
+    NumberOfPosts : int
+    Languges : List[LangugesResponse] = []
