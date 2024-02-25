bfe73deecdae15da6745d9528d147ea3bded6141
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2024-02-25 22:34:52 +0700

    update middleware and change logging -> logger

--- backend/common_service/src/service/taskGetdataset.py
+++ backend/common_service/src/service/taskGetdataset.py
@@ -4,19 +4,17 @@ import logging
 from datetime import datetime
 from apify_client import ApifyClient
 from .BEservice import getLocationIdFromIG, isExistPost, insertPostToInitialData, findLocationId
+from src.middleware.logger import logger
 
 # Replace with your Apify API token and actor ID
 api_token = "apify_api_r06uhUkKIWHwCc0PS0dYIupG4dqBIv2s9D1P" #new token
 actor_id = "RB9HEZitC8hIUXAha"
 
 client = ApifyClient(api_token)
-logger = logging.getLogger('mylogger')
-logger.setLevel(logging.INFO)
-logging.basicConfig(level=logging.INFO)
 
 def getDatasetFromApify():
-    logging.info('call get dataset from apify')
-    logging.info('call get location id from ig')
+    logger.info('call get dataset from apify')
+    logger.info('call get location id from ig')
     listOfLocationFromIG = getLocationIdFromIG()
     for locations in listOfLocationFromIG:
         for location in locations[0] :
@@ -30,11 +28,11 @@ def getDatasetFromApify():
                 "searchLimit": 1,
             }
 
-            logging.info(f'call apify with url {run_input["directUrls"]}')
+            logger.info(f'call apify with url {run_input["directUrls"]}')
             run = client.actor(actor_id).call(run_input=run_input)
             # Fetch and print Actor results from the run's dataset (if there are any)
             for data in client.dataset(run["defaultDatasetId"]).iterate_items():
-                logging.info('call find location id')
+                logger.info('call find location id')
                 location_id = findLocationId(data.get("location_id"))
                 for post in data.get("latestPosts"):
                     post_taken_at = post["taken_at"]
@@ -53,12 +51,12 @@ def getDatasetFromApify():
                     if isExistPost(user_id, post_taken_at) is None :
                         insertPostToInitialData(user_id, username, full_name, caption, post_created_at, post_taken_at, location_id, created_at, created_by)
     
-    logging.info(f'sent date to analytic service date : {datetime.now().date()}')
+    logger.info(f'sent date to analytic service date : {datetime.now().date()}')
     try :
         response = requests.post("http://0.0.0.0:8001/analytics", json={"date" : datetime.now().date()})
         if response.status_code == 200 :
-            logging.info('sent date to analytic is success')
+            logger.info('sent date to analytic is success')
         else :
-            logging.error(f'Error : {response}')
+            logger.error(f'Error : {response}')
     except Exception as e :
-        logging.error(f'Error : {e}')
+        logger.error(f'Error : {e}')
