277b8e76240794dd737709a31d17646cc376fe61
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2024-02-26 21:28:46 +0700

    set asia/bangkok timezone in scheduler

--- backend/common_service/src/service/schedule.py
+++ backend/common_service/src/service/schedule.py
@@ -1,6 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from .taskGetdataset import getDatasetFromApify
 
-scheduler = BackgroundScheduler()
+scheduler = BackgroundScheduler({'apscheduler.timezone': 'Asia/Bangkok'})
 
 scheduler.add_job(getDatasetFromApify, 'cron', day_of_week='mon-sun', minute='1')
